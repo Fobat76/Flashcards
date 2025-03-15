8aadcbf2362c772cfec38f146352bf3f0de982c1
Devin Chen <Devinc23456@gmail.com>
2025-03-14 22:34:37 -0400

    Update

--- README.md
+++ README.md
@@ -6,27 +6,19 @@ Submitted by: **Devin Chen**
 
 Time spent: **3** hours spent in total
 
-## Goals
-- [x] Create a new component  
-- [x] Share a small piece of data from one component to the next  
-- [x] Utilize `useState()` to create state variables to help control component behavior  
-- [x] Use the `onClick()` event to call a method  
-- [x] Create multiple `div` sections to keep track of different chunks of information  
-- [x] Use those `div` sections as the basis for CSS styling  
 ## Required Features
-- [x] The title of the card set and some information about it, such as a short description and the total number of cards are displayed  
-- [x] A single card at a time is displayed, only showing one of the components of the information pair  
-- [x] Create a list of card pairs (an array of dictionaries where each dictionary contains the question and answer is perfectly fine)  
-- [x] Clicking on the card shows the corresponding component of the information pair  
-- [x] Clicking the next button displays a random new card  
-
+[x] User Input
+- The user can enter their guess in a box before seeing the flipside of the card
+- Clicking on a submit button shows visual feedback about whether the answer was correct or incorrect
+[x] Moving Multiple Ways in the Card List
+- A back button displayed on the card can be used to return to the previous card in a set sequence
+= A next button displayed on the card can be used to navigate to the next card in a set sequence
 GIF created with ScreenToGif
-![](https://github.com/Fobat76/Flashcards/blob/main/Pokemon-flashcard.gif)
-![](https://github.com/Fobat76/Flashcards/blob/main/Pokemon-flashcard-reduced.gif)
+![]()
+![]()
 
 ### Describe any challenges encountered while building the app.
-- CSS 
-- Backend stuff
+- Backend edge case
 ## License
 
     Copyright [2025] [Devin Chen]
--- src/components/Flashcards.jsx
+++ src/components/Flashcards.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 
 export default function Flashcards({ data }) {
+
   const [index, setIndex] = useState(0);
   const [show, setShow] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
   function Next() {
-    if (show) {
-        setIndex(Math.floor(Math.random() * data.length));
-        setShow(false);
-    } else {
-        setShow(true);
+    if (index === data.length - 1)  {
+      setIndex(0);
+      setShow(false)
+      return;
     }
-}
+    setIndex(index + 1);
+    setShow(false)
+  }
+  
     function Previous() {
         if (index === 0) {
             setIndex(data.length - 1);
@@ -23,6 +28,34 @@ export default function Flashcards({ data }) {
         backgroundColor: data[index].color
     }
 
+    function Shuffle(){
+      data = data.sort( () => Math.random()-0.5 );
+      setIndex(Math.floor(Math.random() * data.length));
+      setShow(false);
+    }
+
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    }
+
+    function Submit(){
+      if(inputValue == data[index].name){
+        alert("Correct")
+        setInputValue('')
+        setIndex(Math.floor(Math.random() * data.length));
+        setShow(false);
+      }
+      else if(inputValue == ''){
+        alert("Enter Value")
+      }
+      else{
+        alert("Wrong")
+      }
+    }
+
+    function Show(){
+      setShow(!show)
+    }
   return (
     <>
     <div className='Flashcard' style={backgroundcolor}>
@@ -31,8 +64,11 @@ export default function Flashcards({ data }) {
     </div>
     <div className='Flashcard-Buttons'>
         <button onClick={Previous}>Previous</button>
-        <button onClick={Next}>{show ? 'Next' : 'Show'}</button>
-        <h1>Pages {index}</h1>
+        <button onClick={Show}>Show</button>
+        <button onClick={Next}>Next</button>
+        <button onClick={Shuffle}>Shuffle</button>
+        <input type="text" onChange={handleInputChange}></input>
+        <button onClick={Submit}>Submit</button>
     </div>
     </> 
   );
